2c2cf1e07ae74a4aa22c068a07d52ed11049accd
Thors161 <67685381+Thors161@users.noreply.github.com>
2024-10-18 15:24:04 +0200

    check for pump off, empty display

--- scripts/wilo_cam_mqtt.sh
+++ scripts/wilo_cam_mqtt.sh
@@ -17,6 +17,8 @@ power_read2=unknown
 max_tries=10
 tries=0
 
+empty_reads=0
+
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 while [[ $tries -le $max_tries && ("$flow_read" == "false" || "$power_read" == "false") ]]; do
@@ -37,87 +39,98 @@ while [[ $tries -le $max_tries && ("$flow_read" == "false" || "$power_read" == "
     input=$(echo "$input" | sed 's/^[ \t]*//;s/[ \t]*$//')
     echo "Wilo: value: \"$input\""
 
+    #check for empty read (Wilo off)
+    if [ -z "$input" ]; then
+        let empty_reads=empty_reads+1
 
-    # Check if the input is a numerical value with a decimal, and between 0.0 and 3.0
-    if [[ $input =~ ^[0-9]+\.[0-9]+$ ]]; then  # Check if it contains a dot and is numerical
-        # Convert string to a float and check range
-        if (( $(echo "$input >= 0.0" | bc -l) && $(echo "$input <= 3.0" | bc -l) )); then
-
-            if [ "$flow_read1" != "unknown" ] && [ "$flow_read2" != "unknown" ]; then
-                # two different values in place, and a third one read
-                echo "Wilo: Read two different flow values: $flow_read1 $flow_read2"
-
-                if [ "$input" = "$flow_read2" ]; then
-                    flow_read1=$input
-                    echo "Wilo: Valid flow value 1: $input"
-                elif [ "$input" = "$flow_read1" ]; then
-                    flow_read2=$input
-                    echo "Wilo: Valid flow value 2: $input"
-                else
-                    # start over
-                    flow_read1=unknown
-                    flow_read2=unknown
-               fi
-            fi
-
-            if [ "$flow_read1" = "unknown" ]; then
-		flow_read1=$input;
-	        echo "Wilo: Valid flow value 1: $input"
-            elif [ "$flow_read2" = "unknown" ]; then
-                flow_read2=$input;
-                echo "Wilo: Valid flow value 2: $input"
-            fi
-
-	    if [ "$flow_read1" = "$flow_read2" ]; then
-                echo "Wilo: Read two identical flow values: $flow_read1 $flow_read2"
-                flow_read=true
-            fi
-        else
-            echo "Wilo: Invalid flow value: $input"
+        if [[ "$empty_reads" == "3" ]]; then
+            echo "Wilo: off"
+            flow_read=true
+            flow_read1=0 
+            power_read=true
+            power_read1=0
         fi
-        cp /tmp/wilo.jpg /tmp/wilo_flow.jpg
     else
-        # Check if the input is an integer, does not start with 0, and is between 2 and 75
-        if [[ $input =~ ^[1-9][0-9]*$ ]]; then  # Check if it's an integer and doesn't start with 0
-            if [ "$input" -ge 2 ] && [ "$input" -le 75 ]; then  # Check if the integer is between 2 and 75
+        # Check if the input is a numerical value with a decimal, and between 0.0 and 3.0
+        if [[ $input =~ ^[0-9]+\.[0-9]+$ ]]; then  # Check if it contains a dot and is numerical
+            # Convert string to a float and check range
+            if (( $(echo "$input >= 0.0" | bc -l) && $(echo "$input <= 3.0" | bc -l) )); then
 
-                if [ "$power_read1" != "unknown" ] && [ "$power_read2" != "unknown" ]; then
+                if [ "$flow_read1" != "unknown" ] && [ "$flow_read2" != "unknown" ]; then
                     # two different values in place, and a third one read
-                    echo "Wilo: Read two different power values: $power_read1 $power_read2"
+                    echo "Wilo: Read two different flow values: $flow_read1 $flow_read2"
 
-                    if [ "$input" = "$power_read2" ]; then
-                        power_read1=$input
-                        echo "Wilo: Valid power value 1: $input"
-                    elif [ "$input" = "$power_read1" ]; then
-                        power_read2=$input
-                        echo "Wilo: Valid power value 2: $input"
+                    if [ "$input" = "$flow_read2" ]; then
+                        flow_read1=$input
+                        echo "Wilo: Valid flow value 1: $input"
+                    elif [ "$input" = "$flow_read1" ]; then
+                        flow_read2=$input
+                        echo "Wilo: Valid flow value 2: $input"
                     else
                         # start over
-                        power_read1=unknown
-                        power_read2=unknown
+                        flow_read1=unknown
+                        flow_read2=unknown
                    fi
                 fi
 
-                if [ "$power_read1" = "unknown" ]; then
-                    power_read1=$input;
-                    echo "Wilo: Valid power value 1: $input"
-                elif [ "$power_read2" = "unknown" ]; then
-                    power_read2=$input;
-                    echo "Wilo: Valid power value 2: $input"
+                if [ "$flow_read1" = "unknown" ]; then
+	    	flow_read1=$input;
+	            echo "Wilo: Valid flow value 1: $input"
+                elif [ "$flow_read2" = "unknown" ]; then
+                    flow_read2=$input;
+                    echo "Wilo: Valid flow value 2: $input"
                 fi
 
-                if [ "$power_read1" = "$power_read2" ]; then
-                    echo "Wilo: Read two identical power values: $power_read1 $power_read2"
-                    power_read=true
+    	        if [ "$flow_read1" = "$flow_read2" ]; then
+                    echo "Wilo: Read two identical flow values: $flow_read1 $flow_read2"
+                    flow_read=true
                 fi
             else
-                echo "Wilo: Invalid power value: $input"
+                echo "Wilo: Invalid flow value: $input"
             fi
+            cp /tmp/wilo.jpg /tmp/wilo_flow.jpg
+        else
+            # Check if the input is an integer, does not start with 0, and is between 2 and 75
+            if [[ $input =~ ^[1-9][0-9]*$ ]]; then  # Check if it's an integer and doesn't start with 0
+                if [ "$input" -ge 2 ] && [ "$input" -le 75 ]; then  # Check if the integer is between 2 and 75
+
+                    if [ "$power_read1" != "unknown" ] && [ "$power_read2" != "unknown" ]; then
+                        # two different values in place, and a third one read
+                        echo "Wilo: Read two different power values: $power_read1 $power_read2"
+
+                        if [ "$input" = "$power_read2" ]; then
+                            power_read1=$input
+                            echo "Wilo: Valid power value 1: $input"
+                        elif [ "$input" = "$power_read1" ]; then
+                            power_read2=$input
+                            echo "Wilo: Valid power value 2: $input"
+                        else
+                            # start over
+                            power_read1=unknown
+                            power_read2=unknown
+                        fi
+                    fi
+
+                    if [ "$power_read1" = "unknown" ]; then
+                         power_read1=$input;
+                        echo "Wilo: Valid power value 1: $input"
+                    elif [ "$power_read2" = "unknown" ]; then
+                        power_read2=$input;
+                        echo "Wilo: Valid power value 2: $input"
+                    fi
+
+                    if [ "$power_read1" = "$power_read2" ]; then
+                        echo "Wilo: Read two identical power values: $power_read1 $power_read2"
+                        power_read=true
+                    fi
+                else
+                    echo "Wilo: Invalid power value: $input"
+                fi
+             fi
+             cp /tmp/wilo.jpg /tmp/wilo_power.jpg
         fi
-        cp /tmp/wilo.jpg /tmp/wilo_power.jpg
     fi
     ((tries++))
-    #sleep 1
 done
 
 if [ "$flow_read" = "false" ]; then
